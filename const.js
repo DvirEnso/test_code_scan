05d88c8d9c0498485d0d132e7ee01d0ab3ecb165
Dvir <80195431+DvirEnso@users.noreply.github.com>
2021-08-02 10:50:46 +0300

    Update const.js

--- const.js
+++ const.js
@@ -1,3 +1,3 @@
-const AccesskeyID = 'AKIAIOSFODNN7EXAMPLE'
-const SecretAccesskey = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+const accesskeyID = 'AKIAIOSFODNN7EXAMPLE'
+const secretAccesskey = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
 const region = 'us-west-2'
